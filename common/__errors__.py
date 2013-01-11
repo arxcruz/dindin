3b9145d08eaed0440f4e1972833ecd93257b101c
Fabio Dassan dos Santos <fabio@fabiodassan.com>
2013-01-10 22:31:44 -0200

    Create a module to store error messages

---
+++ common/__errors__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Common
+SUCCESS = "Success"
+
+### User controller
+ERROR_BUCKET_NOT_EXIST = "There is no bucket with this given name"
+ERROR_BUCKET_ALREADY_EXISTS = "Unable to create bucket. There is already other one with this given name"
+ERROR_REMOVE_BUCKET_NOT_EXIST = "Unable to remove bucket. There is no bucket with this given name"
+ERROR_USER_DOES_NOT_EXIST = "User does not exist. Abort."
